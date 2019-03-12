288487c9a920173f7b8ec998db2cbe11896dc4cc
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-03-12 14:45:49 +0100

    tilføjet unit test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool a = true;
+            Assert.IsTrue(a);
+        }
+    }
+}
